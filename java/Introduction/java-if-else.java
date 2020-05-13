689089667a759115f90f22653ce4c857680cc699
rajharsh81070 <rajharsh81070@gmail.com>
2020-05-13 17:36:55 +0530

    Added all solutions from Java Introduction

---
+++ java/Introduction/java-if-else.java
@@ -0,0 +1,33 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+  private static final Scanner scanner = new Scanner(System.in);
+
+  public static boolean isOdd(int N) {
+    return N % 2 == 1 ? true : false;
+  }
+
+  public static void main(String[] args) {
+    int N = scanner.nextInt();
+    scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+    scanner.close();
+
+    if (isOdd(N)) {
+      System.out.println("Weird");
+    } else {
+      if (N < 6)
+        System.out.println("Not Weird");
+      else if (N > 5 && N < 21)
+        System.out.println("Weird");
+      else
+        System.out.println("Not Weird");
+    }
+  }
+}
---
+++ java/Introduction/java-static-initializer-block.java
@@ -0,0 +1,14 @@
+static int B;
+    static int H;
+    static boolean flag = true;
+    static {
+        Scanner scan = new Scanner(System.in);
+        B = scan.nextInt();
+        scan.nextLine();
+        H = scan.nextInt();
+        scan.close();
+        if(B<=0 || H<=0) {
+            flag = false;
+            System.out.println("java.lang.Exception: Breadth and height must be positive");
+        }
+    }
