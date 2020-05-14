d2ecba751694cb84320677b0d07f769e60900cb1
rajharsh81070 <rajharsh81070@gmail.com>
2020-05-14 19:29:27 +0530

    Problem #2

---
+++ java/BigNumber/java-primality-test.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+  private static final Scanner scanner = new Scanner(System.in);
+
+  public static void main(String[] args) {
+    BigInteger n = scanner.nextBigInteger();
+    // isPrime = new BigDecimal(n);
+    boolean chk = n.isProbablePrime(1);
+    if (chk)
+      System.out.println("prime");
+    else
+      System.out.println("not prime");
+    scanner.close();
+  }
+}
